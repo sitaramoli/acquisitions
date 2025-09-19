993e30d4cdd239701ef15bcb67153d9f2974d933
SR Oli <sitaramoli1998@gmail.com>
2025-09-18 23:09:10 -0400

    Implement: Testing, CI/CD pipelines and github actions

--- src/app.js
+++ src/app.js
@@ -44,4 +44,11 @@ app.get('/api/', (req, res) => {
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 
+app.use((req, res) => {
+  res.status(404).json({
+    error: 'Not Found',
+    message: 'The requested resource does not exist.',
+  });
+});
+
 export default app;
